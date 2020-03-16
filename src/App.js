6e91383a2e520973521d520da4870e8819ba6821
Amritanshu rai <amritanshurai13@gmal.com>
2020-03-17 02:02:02 +0530

    fetch question data only at page refresh on home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import HomePage from "./pages/homepae/homepage.page";
 import Solution from "./components/solution/solution.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
@@ -10,11 +10,17 @@ import Loader from "./components/loader/loader.component";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.action";
 import { selectCurrentUser } from "./redux/user/user.selector";
+import { setQuestionData } from "./redux/question/question.action";
+import { toggleLoader } from "./redux/universal/universal.action";
+import { firestore } from "./firebase/firebase.utils";
 
 class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
+        if (this.props.location.pathname === "/") {
+            this.fetchData();
+        }
         const { setCurrentUser } = this.props;
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             if (userAuth) {
@@ -29,6 +35,14 @@ class App extends React.Component {
                 setCurrentUser(userAuth);
             }
         });
+    }
+
+    async fetchData() {
+        this.props.toggleLoader();
+        const userRef = firestore.doc(`questions/b41rFEKQw3OOzuzImSci`);
+        const { questions } = await (await userRef.get()).data();
+        this.props.setQuestionData(questions);
+        this.props.toggleLoader();
     }
 
     componentWillUnmount() {
@@ -75,6 +89,8 @@ const mapStateToProps = state => ({
     currentUser: selectCurrentUser(state)
 });
 const mapDispatchToProps = dispatch => ({
-    setCurrentUser: user => dispatch(setCurrentUser(user))
+    setCurrentUser: user => dispatch(setCurrentUser(user)),
+    toggleLoader: () => dispatch(toggleLoader()),
+    setQuestionData: data => dispatch(setQuestionData(data))
 });
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
--- src/components/question-container/question-container.component.jsx
+++ src/components/question-container/question-container.component.jsx
@@ -3,21 +3,8 @@ import "./question-container.styles.scss";
 import Question from "../question/question.component";
 import { connect } from "react-redux";
 import { selectQuestionData } from "../../redux/question/question.selector";
-import { setQuestionData } from "../../redux/question/question.action";
-import { toggleLoader } from "../../redux/universal/universal.action";
-import { firestore } from "../../firebase/firebase.utils";
 
 class QuestionContainer extends React.Component {
-    componentDidMount() {
-        this.fetchData();
-    }
-    async fetchData() {
-        this.props.toggleLoader();
-        const userRef = firestore.doc(`questions/b41rFEKQw3OOzuzImSci`);
-        const { questions } = await (await userRef.get()).data();
-        this.props.setQuestionData(questions);
-        this.props.toggleLoader();
-    }
     render() {
         return (
             <div className="questionContainer">
@@ -31,8 +18,4 @@ class QuestionContainer extends React.Component {
 const mapStateToProps = state => ({
     question_data: selectQuestionData(state)
 });
-const mapDispatchToProps = dispatch => ({
-    toggleLoader: () => dispatch(toggleLoader()),
-    setQuestionData: data => dispatch(setQuestionData(data))
-});
-export default connect(mapStateToProps, mapDispatchToProps)(QuestionContainer);
+export default connect(mapStateToProps)(QuestionContainer);
