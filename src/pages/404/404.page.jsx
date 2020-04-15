e83e923a404e2d5110ec4f85187597a17fca6a4c
Amritanshu rai <amritanshurai13@gmail.com>
2020-04-16 02:46:30 +0530

    404 design changed

--- src/pages/404/404.page.jsx
+++ src/pages/404/404.page.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Result } from 'antd';
+// import { Link } from 'react-router-dom';
 import './404.styles.scss';
 const PageNotFound = () => {
-  return <div className='notFound'>lauda lag gaya</div>;
+  return (
+    <Result
+      status='404'
+      title='404'
+      subTitle='Sorry, the page you visited does not exist.'
+      // extra={
+      //   <Button type='primary'>
+      //     <Link to='/'>Home</Link>
+      //   </Button>
+      // }
+    />
+  );
 };
 
 export default PageNotFound;
