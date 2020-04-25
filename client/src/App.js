a0ba66f5f46d837c6c31fe5d959e80416bf99dcc
Amritanshu rai <amritanshurai13@gmail.com>
2020-04-26 04:15:27 +0530

    app component cleaned

--- client/src/App.js
+++ client/src/App.js
@@ -25,48 +25,10 @@ class App extends React.PureComponent {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    // if (this.props.location.pathname === '/') {
-    //   this.props.toggleLoader(true);
-    //   const fetchedData = await fetchData(this.onFetchedData, {
-    //     toggleLoader: this.props.toggleLoader,
-    //   });
-    //   this.props.setQuestionData(fetchedData);
-    //   this.props.toggleLoader(false);
-    // document.addEventListener('keydown', this.handleKeyPress, false);
-    // }
     const { checkUserSession } = this.props;
     checkUserSession();
   }
-  // onFetchedData = fetchedData => {
-  //   this.props.setQuestionData(fetchedData);
-  //   if (this.props.location.pathname === '/') {
-  //     this.props.toggleLoader(false);
-  //   }
-  // };
-  // async fetchData() {
-  //   this.props.toggleLoader();
-  //   // const userRef = firestore.doc(`questions/b41rFEKQw3OOzuzImSci`);
-  //   // const { questions } = await (await userRef.get()).data();
-  //   let questions = {};
-  //   const value = await firestore
-  //     .collection('questions')
-  //     .orderBy('timestamp', 'desc')
-  //     .get();
-  //   value.forEach(doc => {
-  //     // console.log(`${doc.id} => ${doc.data()}`);
-  //     questions[doc.id] = doc.data();
-  //   });
 
-  //   this.props.setQuestionData(questions);
-
-  //   this.props.toggleLoader();
-  // }
-  // handleKeyPress = event => {
-  //   event.preventDefault();
-  //   if (event.keyCode === 32 && !!event.shiftKey) {
-  //     this.props.history.push('/approve');
-  //   }
-  // };
   componentWillUnmount() {
     this.unsubscribeFromAuth();
     document.removeEventListener('keydown', this.handleKeyPress, false);
--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -33,3 +33,44 @@ export const fetchData = async (collectionName) => {
   });
   return questions;
 };
+
+// if (this.props.location.pathname === '/') {
+//   this.props.toggleLoader(true);
+//   const fetchedData = await fetchData(this.onFetchedData, {
+//     toggleLoader: this.props.toggleLoader,
+//   });
+//   this.props.setQuestionData(fetchedData);
+//   this.props.toggleLoader(false);
+// document.addEventListener('keydown', this.handleKeyPress, false);
+// }
+
+// onFetchedData = fetchedData => {
+//   this.props.setQuestionData(fetchedData);
+//   if (this.props.location.pathname === '/') {
+//     this.props.toggleLoader(false);
+//   }
+// };
+// async fetchData() {
+//   this.props.toggleLoader();
+//   // const userRef = firestore.doc(`questions/b41rFEKQw3OOzuzImSci`);
+//   // const { questions } = await (await userRef.get()).data();
+//   let questions = {};
+//   const value = await firestore
+//     .collection('questions')
+//     .orderBy('timestamp', 'desc')
+//     .get();
+//   value.forEach(doc => {
+//     // console.log(`${doc.id} => ${doc.data()}`);
+//     questions[doc.id] = doc.data();
+//   });
+
+//   this.props.setQuestionData(questions);
+
+//   this.props.toggleLoader();
+// }
+// handleKeyPress = event => {
+//   event.preventDefault();
+//   if (event.keyCode === 32 && !!event.shiftKey) {
+//     this.props.history.push('/approve');
+//   }
+// };
