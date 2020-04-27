a8361b7af3cbf80cf04f29f760c9654af3673c17
Amritanshu rai <amritanshurai13@gmail.com>
2020-04-27 14:37:38 +0530

    question list css modified

--- client/src/components/question/question.component.jsx
+++ client/src/components/question/question.component.jsx
@@ -3,8 +3,8 @@ import './question.styles.scss';
 import { Link } from 'react-router-dom';
 import { EditorState, ContentState, convertFromHTML } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
-
-import { Col, Collapse } from 'antd';
+import { DislikeOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
+import { Col, Collapse, Button, Rate } from 'antd';
 
 const { Panel } = Collapse;
 const someFunc = (htmlStr) => {
@@ -20,7 +20,8 @@ const someFunc = (htmlStr) => {
 };
 
 const styleCol = {
-  minWidth: '350px',
+  width: '50%',
+  minWidth: '400px',
 };
 
 const collapseStyle = {
@@ -28,29 +29,63 @@ const collapseStyle = {
   fontSize: '16px',
   overflowWrap: 'break-word',
 };
-class Question extends React.Component {
-  render() {
-    const {
-      id = 1,
-      question: { title = '', explanation = '' },
-    } = this.props;
 
-    return (
-      <Col style={styleCol} flex='auto'>
-        <Collapse style={collapseStyle}>
-          <Panel header={title} key={id}>
+const Question = (props) => {
+  // const IconText = ({ icon, text }) => (
+  //   <span>
+  //     {React.createElement(icon, { style: { marginRight: 8 } })}
+  //     {text}
+  //   </span>
+  // );
+
+  const {
+    id = 1,
+    question: { title = '', explanation = '' },
+  } = props;
+
+  return (
+    <Col style={styleCol} flex='auto' className='question'>
+      <Collapse style={collapseStyle}>
+        <Panel header={title} key={id}>
+          <Editor toolbarHidden editorState={someFunc(explanation)} readOnly />
+          <div className='f question_footer'>
             <Link className='question_link' to={`/solution/${id}`}>
-              <Editor
-                toolbarHidden
-                editorState={someFunc(explanation)}
-                readOnly
-              />
+              <Button>Solution</Button>
             </Link>
-          </Panel>
-        </Collapse>
-      </Col>
-    );
-  }
-}
+            {/* <Rate character={<HeartOutlined />} allowHalf /> */}
+            <div className='f question_footer-rating'>
+              <span className='f question_footer-icon'>
+                <Rate character={<StarOutlined />} count={1} />
+                <span className='question_footer-number'>{101}</span>
+              </span>
+              <div className='f question_footer-vote'>
+                <span className='f question_footer-icon'>
+                  <Rate character={<DislikeOutlined />} count={1} />
+                  <span className='question_footer-number'>{201}</span>
+                </span>
+
+                <span className='f question_footer-icon'>
+                  <Rate character={<LikeOutlined />} count={1} />
+                  <span className='question_footer-number'>{201}</span>
+                </span>
+              </div>
+            </div>
+            {/* <IconText
+              icon={StarOutlined}
+              text='156'
+              key='list-vertical-star-o'
+            />
+            ,
+            <IconText
+              icon={LikeOutlined}
+              text='156'
+              key='list-vertical-like-o'
+            /> */}
+          </div>
+        </Panel>
+      </Collapse>
+    </Col>
+  );
+};
 
 export default Question;
