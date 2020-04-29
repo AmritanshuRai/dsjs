609f90b6384b20d8ca31edc8b1be6e47b36005fb
Amritanshu rai <amritanshurai13@gmail.com>
2020-04-29 12:07:39 +0530

    test

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,18 @@
 import './wdyr';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
+
 // import store from "./redux/store";
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+import App from './App';
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
